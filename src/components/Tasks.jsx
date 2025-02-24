d4493eac459e4e509e70a90a4fd3d76676418111
yanocosta02 <yancosta2002@gmail.com>
2025-02-24 16:37:29 -0300

    check

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, Trash2Icon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -21,9 +21,10 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
           <button
             onClick={() => onTaskClick(task.id)}
             className={
-              "bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'}"
+              "bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${task.isCompleted && 'line-through'}"
             }
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
